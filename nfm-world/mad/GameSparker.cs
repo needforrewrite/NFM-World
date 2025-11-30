98562d5e3f6a3c1e66671992459b8be54ff0c523
Mawskeeto1223 <tomaxx11@gmail.com>
2025-11-30 04:12:59 +0200

    test stage

--- nfm-world/mad/GameSparker.cs
+++ nfm-world/mad/GameSparker.cs
@@ -8,10 +8,23 @@ public class GameSparker
 {
     private static MicroStopwatch timer;
     private static ContO[] cars;
+    private static ContO[] stage_parts;
     private static CarState[] current_car_states;
     private static CarState[] prev_car_states;
 
-    private static readonly string[] CarRads = { "2000tornados" };
+    private static readonly string[] CarRads = {
+        "2000tornados", "formula7", "canyenaro", "lescrab", "nimi", "maxrevenge", "leadoxide", "koolkat", "drifter",
+        "policecops", "mustang", "king", "audir8", "masheen", "radicalone", "drmonster"
+    };
+    public static readonly string[] StageRads = {
+        "road", "froad", "twister2", "twister1", "turn", "offroad", "bumproad", "offturn", "nroad", "nturn",
+        "roblend", "noblend", "rnblend", "roadend", "offroadend", "hpground", "ramp30", "cramp35", "dramp15",
+        "dhilo15", "slide10", "takeoff", "sramp22", "offbump", "offramp", "sofframp", "halfpipe", "spikes", "rail",
+        "thewall", "checkpoint", "fixpoint", "offcheckpoint", "sideoff", "bsideoff", "uprise", "riseroad", "sroad",
+        "soffroad", "tside", "launchpad", "thenet", "speedramp", "offhill", "slider", "uphill", "roll1", "roll2",
+        "roll3", "roll4", "roll5", "roll6", "opile1", "opile2", "aircheckpoint", "tree1", "tree2", "tree3", "tree4",
+        "tree5", "tree6", "tree7", "tree8", "cac1", "cac2", "cac3", "8sroad", "8soffroad"
+    };
 
     private static long accumulator = 0;
     private static long lastFrameTime = 0;
@@ -142,7 +155,7 @@ public static void KeyPressed(Keys key)
     {
         Trackers.Devidetrackers(100, 100, 100, 100);
 
-        playerMad = new Mad(new Stat(0), 0);
+        playerMad = new Mad(new Stat(14), 0);
         playerControl = new Control();
         timer = new MicroStopwatch();
         timer.Start();
@@ -153,25 +166,91 @@ public static void KeyPressed(Keys key)
         Medium.Groundpolys();
         Medium.D();
         
-        cars = new ContO[10];
-        current_car_states = new CarState[10];
-        prev_car_states = new CarState[10];
+        cars = new ContO[100];
+        stage_parts = new ContO[100];
 
-        for(int i = 0; i < 10; i++)
-        {
+        current_car_states = new CarState[100];
+        prev_car_states = new CarState[100];
+
+        for(int i = 0; i < 100; i++) {
             current_car_states[i] = new CarState();
             prev_car_states[i] = new CarState();
         }
 
-        FileUtil.LoadFiles("./data/cars", CarRads, (ais, id) =>
-        {
+        FileUtil.LoadFiles("./data/models/cars", CarRads, (ais, id) => {
             cars[id] = new ContO(ais);
-            cars[id] = new ContO(cars[id], 0, 0, 0, 0);
             if (!cars[id].Shadow)
             {
                 throw new Exception("car does not have a shadow");
             }
         });
+
+        FileUtil.LoadFiles("./data/models/stage", StageRads, (ais, id) => {
+            stage_parts[id] = new ContO(ais);
+        });
+
+
+        // part gallery stage, kill asap when proper stage loading is done
+        cars[0] = new ContO(cars[14], 0, 0, 0, 0);
+
+        stage_parts[0] = new ContO(stage_parts[37], 0, 250, 0, 0);
+        stage_parts[1] = new ContO(stage_parts[0], 0, 250, 5600, 0);
+        stage_parts[2] = new ContO(stage_parts[16], 0, 250, 3400, 0);
+        stage_parts[3] = new ContO(stage_parts[13], 0, 250, 8400, 180);
+        stage_parts[4] = new ContO(stage_parts[13], 0, 250, -2800, 0);
+        //its 4:08 am, idk why the fuck it keeps repeating the first 5 pieces wtf
+        stage_parts[5] = new ContO(stage_parts[0], 0, 250, 18000, 0);
+        stage_parts[6] = new ContO(stage_parts[1], 3000, 250, 18000, 0);
+        stage_parts[7] = new ContO(stage_parts[2], 6000, 250, 18000, 0);
+        stage_parts[8] = new ContO(stage_parts[3], 9000, 250, 18000, 0);
+        stage_parts[9] = new ContO(stage_parts[4], 12000, 250, 18000, 0);
+        stage_parts[10] = new ContO(stage_parts[5], 15000, 250, 18000, 0);
+        stage_parts[11] = new ContO(stage_parts[6], 18000, 250, 18000, 0);
+        stage_parts[12] = new ContO(stage_parts[7], 21000, 250, 18000, 0);
+        stage_parts[13] = new ContO(stage_parts[8], 24000, 250, 18000, 0);
+        stage_parts[14] = new ContO(stage_parts[9], 27000, 250, 18000, 0);
+        stage_parts[15] = new ContO(stage_parts[10], 30000, 250, 18000, 0);
+        stage_parts[16] = new ContO(stage_parts[11], 33000, 250, 18000, 0);
+        stage_parts[17] = new ContO(stage_parts[12], 36000, 250, 18000, 0);
+        stage_parts[18] = new ContO(stage_parts[13], 39000, 250, 18000, 0);
+        stage_parts[19] = new ContO(stage_parts[14], 42000, 250, 18000, 0);
+        stage_parts[20] = new ContO(stage_parts[15], 45000, 250, 18000, 0);
+        stage_parts[21] = new ContO(stage_parts[16], 48000, 250, 18000, 0);
+        stage_parts[22] = new ContO(stage_parts[17], 51000, 250, 18000, 0);
+        stage_parts[23] = new ContO(stage_parts[18], 54000, 250, 18000, 0);
+        stage_parts[24] = new ContO(stage_parts[19], 57000, 250, 18000, 0);
+        stage_parts[25] = new ContO(stage_parts[20], 60000, 250, 18000, 0);
+        stage_parts[26] = new ContO(stage_parts[21], 63000, 250, 18000, 0);
+        stage_parts[27] = new ContO(stage_parts[22], 66000, 250, 18000, 0);
+        stage_parts[28] = new ContO(stage_parts[23], 69000, 250, 18000, 0);
+        stage_parts[29] = new ContO(stage_parts[24], 72000, 250, 18000, 0);
+        stage_parts[30] = new ContO(stage_parts[25], 75000, 250, 18000, 0);
+        stage_parts[31] = new ContO(stage_parts[26], 78000, 250, 18000, 0);
+        stage_parts[32] = new ContO(stage_parts[27], 81000, 250, 18000, 0);
+        stage_parts[33] = new ContO(stage_parts[28], 84000, 250, 18000, 0);
+        stage_parts[34] = new ContO(stage_parts[29], 87000, 250, 18000, 0);
+        stage_parts[35] = new ContO(stage_parts[30], 90000, 250, 18000, 0);
+        stage_parts[36] = new ContO(stage_parts[31], 93000, 250, 18000, 0);
+        stage_parts[37] = new ContO(stage_parts[32], 96000, 250, 18000, 0);
+        stage_parts[38] = new ContO(stage_parts[33], 99000, 250, 18000, 0);
+        stage_parts[39] = new ContO(stage_parts[34], 102000, 250, 18000, 0);
+        stage_parts[40] = new ContO(stage_parts[35], 105000, 250, 18000, 0);
+        stage_parts[41] = new ContO(stage_parts[36], 108000, 250, 18000, 0);
+        stage_parts[42] = new ContO(stage_parts[37], 111000, 250, 18000, 0);
+        stage_parts[43] = new ContO(stage_parts[38], 114000, 250, 18000, 0);
+
+
+        for (var i = 0; i < StageRads.Length; i++) {
+            if (stage_parts[i] == null) {
+                throw new Exception("No valid ContO (Stage Part) has been assigned to ID " + i + " (" + StageRads[i] + ")");
+            }
+        }
+        for (var i = 0; i < CarRads.Length; i++) {
+            if (cars[i] == null)
+            {
+                throw new Exception("No valid ContO (Vehicle) has been assigned to ID " + i + " (" + StageRads[i] + ")");
+            }
+        }
     }
 
     public static void GameTick()
@@ -225,6 +304,15 @@ public static void KeyPressed(Keys key)
     private static void Render()
     {
         Medium.D();
+
+        // needs a proper render sorter, there's a method called renderObjects from nfm world (nfm-lit/experimental branch) that might help
+        for (int i = 0; i < 44; i++)
+        {
+            if (stage_parts[i] != null)
+            {
+                stage_parts[i].D();
+            }
+        }
         cars[0].D();
     }
 }
