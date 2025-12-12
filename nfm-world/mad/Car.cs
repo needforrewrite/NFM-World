4427d1a9ec87359eeaec03de515de311c317e495
uwx <maxine@cybergal.com>
2025-12-12 04:45:27 +0100

    Car changes for render rewrite

--- nfm-world/mad/Car.cs
+++ nfm-world/mad/Car.cs
@@ -1,23 +1,25 @@
-﻿namespace NFMWorld.Mad;
+﻿using Stride.Core.Mathematics;
+
+namespace NFMWorld.Mad;
 
 public class Car
 {
-    public ContO Conto;
+    public Mesh Conto;
     public Mad Mad;
     public Stat Stat;
     public Control Control;
 
-    public Car(Stat stat, int im, ContO carConto, int x, int z)
+    public Car(Stat stat, int im, Mesh carConto, int x, int z)
     {
-        Conto = new ContO(carConto, x, Medium.Ground, z, 0);
+        Conto = new Mesh(carConto, new Vector3(x, World.Ground, z), Euler.Identity);
         Mad = new Mad(stat, im);
         Stat = stat;
-        Mad.Reseto(im, Conto);
+        // Mad.Reseto(im, Conto);
         Control = new Control();
     }
 
     public void Drive()
     {
-        Mad.Drive(Control, Conto);
+        // Mad.Drive(Control, Conto);
     }
 }
