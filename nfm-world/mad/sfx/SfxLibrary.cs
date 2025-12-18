321bc8aba54900588972bcdc65e82f924672637d
Jacherr <jwc2002@outlook.com>
2025-12-18 11:17:53 +0000

    add more sfx

--- nfm-world/mad/sfx/SfxLibrary.cs
+++ nfm-world/mad/sfx/SfxLibrary.cs
@@ -8,6 +8,13 @@ public static class SfxLibrary
     public static ISoundClip[] skid = new ISoundClip[3];
     public static ISoundClip[] dustskid = new ISoundClip[3];
     public static ISoundClip[] scrape = new ISoundClip[4];
+    public static ISoundClip[] air = new ISoundClip[6];
+
+    public static ISoundClip[,] engs = new ISoundClip[5,5]; // [engine, level]
+
+    public static ISoundClip? wastd;
+    public static ISoundClip? firewasted;
+
 
     public static ISoundClip? tires;
 
@@ -36,6 +43,43 @@ public static class SfxLibrary
         scrape[3] = IBackend.Backend.GetSound("data/sound/scrape2.wav");
 
         tires = IBackend.Backend.GetSound("data/sound/tires.wav");
+
+        for (int engine = 0; engine < 5; engine++)
+        {
+            for (int level = 0; level < 5; level++)
+            {
+                string path = GetEngineSoundPath(GetEngineSignature(engine), level);
+                engs[engine, level] = IBackend.Backend.GetSound(path);
+            }
+        }
+
+        wastd = IBackend.Backend.GetSound("data/sound/wasted.wav");
+        firewasted = IBackend.Backend.GetSound("data/sound/firewasted.wav");
+
+        air[0] = IBackend.Backend.GetSound("data/sound/air0.wav");
+        air[1] = IBackend.Backend.GetSound("data/sound/air1.wav");
+        air[2] = IBackend.Backend.GetSound("data/sound/air2.wav");
+        air[3] = IBackend.Backend.GetSound("data/sound/air3.wav");
+        air[4] = IBackend.Backend.GetSound("data/sound/air4.wav");
+        air[5] = IBackend.Backend.GetSound("data/sound/air5.wav");
 #endif
+    }
+
+    private static string GetEngineSignature(int engine)
+    {
+        return engine switch
+        {
+            0 => "normal",
+            1 => "v8",
+            2 => "retro",
+            3 => "power",
+            4 => "diesel",
+            _ => "normal",
+        };
+    }
+
+    private static string GetEngineSoundPath(string engineSignature, int level)
+    {
+        return $"data/sound/engine/{engineSignature}/{level}.wav";
     }
 }
