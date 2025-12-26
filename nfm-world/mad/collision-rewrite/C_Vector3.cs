baa51a124d316806a0c29b09897e10d3cec5d9e6
Jacherr <jwc2002@outlook.com>
2025-12-26 12:53:41 +0000

    follow c# naming conventions

--- nfm-world/mad/collision-rewrite/C_Vector3.cs
+++ nfm-world/mad/collision-rewrite/C_Vector3.cs
@@ -2,16 +2,11 @@ using SoftFloat;
 
 namespace NFMWorld.Mad.Collision;
 
-public class C_Vector3 {
-    public fix64 x, y, z;
+public class C_Vector3(fix64 x, fix64 y, fix64 z)
+{
+    public fix64 x = x, y = y, z = z;
 
-    public C_Vector3(fix64 x, fix64 y, fix64 z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public C_Vector3 scale(fix64 a) {
+    public C_Vector3 Scale(fix64 a) {
         return new C_Vector3(
             x * a,
             y * a,
@@ -19,7 +14,7 @@ public class C_Vector3 {
         );
     }
 
-    public C_Vector3 translate(C_Vector3 t) {
+    public C_Vector3 Translate(C_Vector3 t) {
         return new C_Vector3(
             x + t.x,
             y + t.y,
@@ -27,7 +22,7 @@ public class C_Vector3 {
         );
     }
 
-    public C_Vector3 rotateZY(fix64 zy) {
+    public C_Vector3 RotateZY(fix64 zy) {
         fix64 a = zy * fix64.DegToRad;
         return new C_Vector3(
             x,
@@ -36,7 +31,7 @@ public class C_Vector3 {
         );
     }
 
-    public C_Vector3 rotateXZ(fix64 xz) {
+    public C_Vector3 RotateXZ(fix64 xz) {
         fix64 a = -xz * fix64.DegToRad;
         return new C_Vector3(
             x * fix64.Cos(a) + z * fix64.Sin(a),
@@ -45,23 +40,23 @@ public class C_Vector3 {
         );
     }
 
-    public fix64 dot(C_Vector3 v) {
+    public fix64 Dot(C_Vector3 v) {
         return x * v.x + y * v.y + z * v.z;
     }
 
-    public fix64 length() {
+    public fix64 Length() {
         return fix64.Sqrt(x * x + y * y + z * z);
     }
 
-    public fix64[] coefficients(C_Vector3 v1, C_Vector3 v2) {
+    public fix64[] Coefficients(C_Vector3 v1, C_Vector3 v2) {
         // A^T A terms
-        fix64 a11 = v1.dot(v1);
-        fix64 a12 = v1.dot(v2);
-        fix64 a22 = v2.dot(v2);
+        fix64 a11 = v1.Dot(v1);
+        fix64 a12 = v1.Dot(v2);
+        fix64 a22 = v2.Dot(v2);
 
         // A^T b terms
-        fix64 b1 = v1.dot(this);
-        fix64 b2 = v2.dot(this);
+        fix64 b1 = v1.Dot(this);
+        fix64 b2 = v2.Dot(this);
 
         fix64 det = a11 * a22 - a12 * a12;
         if (fix64.Abs(det) < (fix64)1e-9f) {
